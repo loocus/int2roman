76a25134cac02eec95135b3d6bd4886cd13c2c5f
loocus <18071484539@163.com>
2023-04-08 21:47:02 +0800

    test: 测试用例补充

--- src/__tests__/intToroman.spec.ts
+++ src/__tests__/intToroman.spec.ts
@@ -1,8 +1,10 @@
 import { isInt, intToRoman } from '../lib/intToRoman';
+import { ROMAN_LIST, INT_LIST } from '../lib/mapper';
 
-describe('romanToInt', () => {
+describe('intToRoman', () => {
   test('isInt 预期返回 true', () => {
     expect(isInt(1)).toBeTruthy();
+    expect(isInt(9999)).toBeTruthy()
   });
 
   test('isInt 预期返回 false', () => {
@@ -28,5 +30,9 @@ describe('romanToInt', () => {
       console.log(`${key} => ${intToRoman(key)}`)
       expect(intToRoman(key)).toEqual(value);
     })
+  })
+
+  test('内部阿拉伯数字预期映射为对应罗马数字', () => {
+    INT_LIST.forEach((integer, i) => expect(intToRoman(integer)).toBe(ROMAN_LIST[i]));
   })
 })
--- src/__tests__/romanToInt.spec.ts
+++ src/__tests__/romanToInt.spec.ts
@@ -1,4 +1,5 @@
 import { isRoman, romanToInt } from '../lib/romanToInt';
+import { INT_LIST, ROMAN_LIST } from '../lib/mapper';
 
 describe('romanToInt', () => {
   const natural = [
@@ -45,11 +46,15 @@ describe('romanToInt', () => {
     unNatural.forEach(str => expect(isRoman(str)).toBeFalsy());
   });
 
-  test('romanToInt 预期返回整数', () => {
+  test('romanToInt 预期返回大于 0 的整数', () => {
     natural.forEach(str => {
       const num = romanToInt(str);
       console.log(`${str} => ${num}`);
       expect(num).toBeGreaterThan(0)
     })
+  });
+
+  test('内部罗马数字预期映射为对应阿拉伯数字', () => {
+    ROMAN_LIST.forEach((roman, i) => expect(romanToInt(roman)).toBe(INT_LIST[i]))
   })
 })
